dace2c49fd99bb659d7026f84afc8f0c83f877d7
SnehaS001 <snehasharma.108001@gmail.com>
2025-06-27 01:44:24 +0530

    Config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+EMAIL_ALERTS = False
+THRESHOLD_PACKET_COUNT = 100
+THRESHOLD_UNIQUE_PORTS = 20
+MONITOR_INTERVAL = 10
+
+EMAIL_CONFIG = {
+    "sender": "your_email@example.com",
+    "password": "your_email_password",
+    "receiver": "alert_receiver@example.com",
+    "smtp_server": "smtp.gmail.com",
+    "smtp_port": 587
+}
